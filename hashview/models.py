ae9490dc3f984b911fa8415d2a5b677f90682cb4
i128 <jarsnah12@gmail.com>
2021-08-15 20:51:21 -0400

    Added ability to edit jobs, and go 'back' during the job creation process

--- hashview/models.py
+++ hashview/models.py
@@ -51,12 +51,9 @@ class Jobs(db.Model):
     status = db.Column(db.String(20), nullable=False)               # Running, Paused, Completed, Queued, Canceled, Ready, Incomplete
     started_at = db.Column(db.DateTime, nullable=True)    # These defaults should be changed
     ended_at = db.Column(db.DateTime, nullable=True)      # These defaults should be changed
-    #hashfile_id = db.Column(db.Integer, db.ForeignKey('hashfiles.id'), nullable=False) # has to not be a foreign key since during job creation, hashfile_id doesnt exist, unless we want to create a dummy one?
     hashfile_id = db.Column(db.Integer, nullable=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    #notify_completed = db.Column(db.Boolean, nullable=False, default=False)
-    #jobtasks = db.relationship('JobTasks', backref='tbd', lazy=True)
 
 class JobTasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -65,28 +62,23 @@ class JobTasks(db.Model):
     command = db.Column(db.String(1024))                  
     status = db.Column(db.String(50), nullable=False)       # Running, Paused, Not Started, Completed, Queued, Canceled, Importing
     agent_id = db.Column(db.Integer, db.ForeignKey('agents.id'))
-    # run_time = db.Column(db.Integer, nullable=False, Default=0)   # This should probably be removed and instead update hashfiles runtime
-    # keyspace_pos = db.Column(db.BigInteger, nullable=False)
-    # keyspace = db.Column(db.BigInteger, nullable=False)
 
 class Customers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
-    #wl_id = db.Column(db.Integer, db.ForeignKey('wordlists.id'), nullable=False) # for dynamic wordlist. Keep?
 
 class Hashfiles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), nullable=False)        # can probably be reduced
     uploaded_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    #wl_id = db.Column(db.Integer, db.ForeignKey('wordlists.id'), nullable=False) # for dynamic wordlist. Keep?
     runtime = db.Column(db.Integer, default=0)
     customer_id = db.Column(db.Integer, nullable=False)
     owner_id = db.Column(db.Integer, nullable=False)
 
 class HashfileHashes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    hash_id = db.Column(db.Integer, nullable=False)
-    username = db.Column(db.String(256), nullable=True, default=None)
+    hash_id = db.Column(db.Integer, nullable=False, index=True)
+    username = db.Column(db.String(256), nullable=True, default=None, index=True)
     hashfile_id = db.Column(db.Integer, nullable=False)
 
 class Agents(db.Model):
@@ -116,7 +108,6 @@ class Wordlists(db.Model):
     last_updated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     type = db.Column(db.String(7))                          # Dynamic or Static
-    #scope = db.Column(db.String(10), nullable=False)
     path = db.Column(db.String(245), nullable=False)
     size = db.Column(db.Integer, nullable=False)
     checksum = db.Column(db.String(64), nullable=False)
@@ -137,24 +128,13 @@ class TaskGroups(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     tasks = db.Column(db.String(256), nullable=False)
 
-# I dont think we use this table
-class TaskQueues(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    jobtask_id = db.Column(db.Integer, db.ForeignKey('jobtasks.id'), nullable=False)
-    job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'), nullable=False)
-    agent_id = db.Column(db.Integer, db.ForeignKey('agents.id'))
-    last_updated = db.Column(db.DateTime, nullable=False)
-    # queued_at # is this neccessary?
-    status = db.Column(db.String(20), nullable=False)   # Running, Completed, Queued, Canceled, Paused
-    command = db.Column(db.String(256), nullable=False) # is command necessary?
-
 class Hashes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sub_ciphertext = db.Column(db.String(32), nullable=False, index=True)
     ciphertext = db.Column(db.String(4096), nullable=False)
-    hash_type = db.Column(db.Integer, nullable=False)
+    hash_type = db.Column(db.Integer, nullable=False, index=True)
     cracked = db.Column(db.Boolean, nullable=False)
-    plaintext = db.Column(db.String(256))
+    plaintext = db.Column(db.String(256), index=True)
 
 class JobNotifications(db.Model):
     id = db.Column(db.Integer, primary_key=True)
